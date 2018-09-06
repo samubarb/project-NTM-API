c9c8b221b6c664b2116c507127785b08a437c233
samubarb <samuele.barbieri95@gmail.com>
2018-09-06 15:35:57 +0200

    add necessaries structs & typedefs

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+/* DEFINES */
+
+#define LEFT 'L'
+#define  RIGHT 'R'
+#define STOP 'S'
+
+/* STRUCTS & TYPEDEFS */
+typedef struct NODE node;
+typedef struct NODE_LIST_ELEMENT node_list_element;
+typedef struct TRANSITION transition;
+
+typedef node_list_element * node_list_ptr;
+typedef node * node_ptr;
+
+struct TRANSITION {
+    unsigned int state_number;
+    char read;
+    char write;
+    char move;
+};
+
+struct NODE {
+    transition t;
+    node_list_ptr children;
+};
+
+struct NODE_LIST_ELEMENT {
+    node ciao;
+    node_ptr prev;
+    node_ptr next;
+};
+
+
+/* GLOBAL VARIABLES & CONSTANTS */
+
+/* PROTOTYPES */
+
+/* MAIN*/
+
+int main (int argc, char *argv[]) {
+    printf ("Hello, World!\n");
     return 0;
 }
+
+/* FUNCTIONS & PROCEDURES */
