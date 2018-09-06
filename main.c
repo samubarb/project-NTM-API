4d92b5ff8a684a9aa00ae94e619bd58718ad710b
samubarb <samuele.barbieri95@gmail.com>
2018-09-06 16:49:26 +0200

    add input command parser

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
 
 /* DEFINES */
 
 #define LEFT 'L'
 #define  RIGHT 'R'
 #define STOP 'S'
+
+#define TR "tr"
+#define ACC "acc"
+#define MAX "max"
+#define RUN "run"
+
+#define EQUALS 0
+#define FIRST_ARG 1
 
 /* STRUCTS & TYPEDEFS */
 typedef struct NODE node;
@@ -33,16 +42,43 @@ struct NODE_LIST_ELEMENT {
     node_ptr next;
 };
 
+enum input_state {Tr, Acc, Max, Run, Data}; // Tr: transitions, Acc: acceptance
+
 
 /* GLOBAL VARIABLES & CONSTANTS */
 
 /* PROTOTYPES */
+enum input_state inputManager(char * input);
+
+
 
 /* MAIN*/
 
 int main (int argc, char *argv[]) {
-    printf ("Hello, World!\n");
-    return 0;
+
+    int i; // indxes
+    enum input_state state;
+
+    for (i = FIRST_ARG; i < argc; i++) {
+        state = inputManager(argv[i]);
+        printf("State: %s\n", state == Tr ? "Transition" : state == Acc ? "Acceptance" : state == Max? "Max" : state == Run ? "Run" : "Data");
+    }
 }
 
 /* FUNCTIONS & PROCEDURES */
+
+enum input_state inputManager(char * input) {
+    if (EQUALS == strcmp(input, TR))
+        return Tr;
+
+    if (EQUALS == strcmp(input, ACC))
+        return Acc;
+
+    if (EQUALS == strcmp(input, MAX))
+        return Max;
+
+    if (EQUALS == strcmp(input, RUN))
+        return Run;
+
+    return Data;
+}
